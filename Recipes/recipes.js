9b9396c5a8169e3398e2438e2157c778abe2de5d
adlingtb <adlingtb@gmail.com>
2022-05-31 00:47:57 +0100

    Working recipe favourites using localstorage

--- Recipes/recipes.js
+++ Recipes/recipes.js
@@ -1,21 +1,128 @@
+let starStyle = "regular";
+
+
 async function getRandomMeal(){
     const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
     const recipe = await response.json();
     const meal = recipe.meals[0];
     console.log(meal);
 
-
     const rndMealEl = document.getElementById("rndMeal");
+    
+
+
 
     rndMealEl.innerHTML = `
     
     <div class="rndMealLabel">Random Meal</div>
+    <a id="star" class="rndMealLabel">
+        <i  class="fa-${starStyle} fa-star"></i>
+    </a>
+    <a id="newMeal" class="rndMealLabel">
+        <i  class="fa-solid fa-arrows-rotate"></i>
+    </a>
     <div class="rndMeal">
         <img src="${meal.strMealThumb}" alt="${meal}">
-        <h2>${meal.strMeal}</h2>
+        <div class="centerVertical">
+            <h2>${meal.strMeal}</h2>
+
+        </div>
     </div>
 
     
     `
+
+    const starEl = document.getElementById("star");
+    const newMealEl = document.getElementById("newMeal");
+
+    starEl.addEventListener("click", function(){
+        if(starStyle === "regular"){
+            starStyle = "solid"
+            addLS(meal.idMeal)
+        }else{
+            starStyle = "regular"
+            removeLS(meal.idMeal)
+        }
+        starEl.innerHTML = "";
+        let i = starEl.appendChild(document.createElement("i")) 
+        i.innerHTML = `
+        <i  class="fa-${starStyle} fa-star"></i>
+        `
+    })
+
+    newMealEl.addEventListener("click", function(){
+        starStyle = "regular";
+        getRandomMeal()
+    });
 }
+
+function addLS(id){
+    let currentLS = JSON.parse(localStorage.getItem("favs"));
+    let allFavs= [id]; 
+    let merge;
+    
+    if(currentLS === null){
+        merge = allFavs;
+    }else{
+        merge = [].concat(currentLS, allFavs);
+    }
+    localStorage.setItem("favs", JSON.stringify(merge));
+    
+    updateFav();
+
+}
+
+function removeLS(id){
+    let currentLS = JSON.parse(localStorage.getItem("favs"));
+
+    for(let i=0; i<currentLS.length; i++){
+        if(currentLS[i]===id){
+            currentLS.splice(i,1)
+        }
+    }
+
+    localStorage.setItem("favs", JSON.stringify(currentLS));
+
+    updateFav();
+
+
+}
+
+
+
+
+async function updateFav(){
+    let currentLS = JSON.parse(localStorage.getItem("favs"));
+    let favEl = document.getElementById("fav");
+
+
+    favEl.innerHTML = "";
+    for(let i = 0; i<currentLS.length; i++){
+
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${currentLS[i]}`);
+        const recipe = await response.json();
+        const meal = recipe.meals[0];
+
+
+        let div = favEl.appendChild(document.createElement("div")) 
+        div.innerHTML = `
+        <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+        <h2>${meal.strMeal}</h2>
+        `
+    }
+
+
+
+
+}
+
+
+
 getRandomMeal();
+updateFav();
+
+
+
+
+
+
